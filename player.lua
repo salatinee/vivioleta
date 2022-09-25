7ebc289b75e8186a526a0dfc223e73130df87eff
salatinee <sawurora@gmail.com>
2022-09-25 04:10:37 -0300

    Initial player idle front animation

--- player.lua
+++ player.lua
@@ -1,12 +1,37 @@
 player = {}
 
 function player:load()
-    self.x = 0
-    self.y = 0
-    self.width = 20
-    self.height = 20
+    self.x = 100
+    self.y = 100
+    self.width = 58
+    self.height = 92
+    self.scale = 0.4
+    local idleFront = love.graphics.newImage("sprites/newIdleFront.png")
+    self.spriteSheets = {
+
+        front = {
+            idle = idleFront,
+            grid = anim8.newGrid(58, 92, idleFront:getWidth(), idleFront:getHeight())
+        },
+
+        back = {
+
+        },
+
+        left = {
+
+        },
+
+        right = {
+            
+        },
+    }
+
+    self.animations = {
+        front = anim8.newAnimation(self.spriteSheets.front.grid('1-2', 1), 0.5)
+    }
     self.speed = 75
-    self.collider = world:newRectangleCollider(self.x, self.y, self.width, self.height)
+    self.collider = world:newRectangleCollider(self.x, self.y, self.width * self.scale, self.height * self.scale)
     self.collider:setFixedRotation(true)
 end
 
@@ -18,6 +43,7 @@ end
 function player:move(dt)
     local vx = 0
     local vy = 0
+    local isMoving = false
     if love.keyboard.isDown("w") then
         vy = -self.speed
     end
@@ -34,14 +60,25 @@ function player:move(dt)
         vx = self.speed
     end
 
+    if vx ~= 0 or vy ~= 0 then
+        isMoving = true
+    end
+
     self.collider:setLinearVelocity(vx, vy)
+    if not isMoving then
+        self.animations.front:update(dt)
+    end
 end
 
 function player:updateCollider()
-    self.x = self.collider:getX() - self.width / 2
-    self.y = self.collider:getY() - self.height / 2
+    self.x = self.collider:getX() - self.width * self.scale / 2
+    self.y = self.collider:getY() - self.height * self.scale / 2
+end
+
+function player:getDimensions()
+    return self.x * scale, self.y * scale
 end
 
 function player:draw()
-    love.graphics.rectangle("line", self.x, self.y, self.height, self.width)
+    self.animations.front:draw(self.spriteSheets.front.idle, self.x, self.y, nil, self.scale, self.scale)
 end
--- requireAll.lua
+++ requireAll.lua
@@ -1,6 +1,9 @@
 function requireAll()
     wf = require("libraries/windfield")
     sti = require("libraries/sti")
+    camera = require("libraries/camera")
+    anim8 = require("libraries/anim8")
     require("map")
     require("player")
+    require("playerCamera")
 end

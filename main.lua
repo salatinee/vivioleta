968e2f83e65d1a3910149c8baa2b9decc6ef02a0
salatinee <sawurora@gmail.com>
2022-10-01 19:50:07 -0300

    fullscreen and quit game implementation

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 require("requireAll")
 requireAll()
 
+math.randomseed(os.time())
+
 function love.load()
     love.window.setMode(624, 624)
     love.graphics.setDefaultFilter("nearest", "nearest")
     world = wf.newWorld(0, 0)
     scale = 3.5
-
+    fullscreen = false
     _G.viviMap = loadTiledMap("viviMap")
     map:load()
     player:load()
@@ -25,6 +27,15 @@ end
 function love.keypressed(key, scancode, isrepeat)
     if key == "e" then
         player:interactWithNPC()
+    end
+
+    if key == "escape" then
+        love.event.quit()
+    end
+
+    if key == "f1" then
+        fullscreen = not fullscreen
+        love.window.setFullscreen(fullscreen)
     end
 end
 
